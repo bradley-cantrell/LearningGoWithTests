71312af25f82f0d1149c1c322a97f4dd686b9899
bradley-cantrell <cantrellbldev@gmail.com>
2023-05-14 19:05:54 -0400

    stopped at hello world - switch section

--- hello-world/Hello.go
+++ hello-world/Hello.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name + "!"
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if lang == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if lang == french {
+		return frenchHelloPrefix + name
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Brad"))
+	fmt.Println(Hello("Brad", ""))
 }
